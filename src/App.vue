0c2f1381dfa255e1f34d0289761d60e4f885ad76
BigBoyBODIA <vbifynty@yandex.ru>
2024-10-19 22:39:00 +0300

    создал псевдозапрос на сторонне api (Данил, посмотри)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,46 @@
 <template>
-  <div class="workAreaImportImage">
-    <button v-if="!isItDefaultImage" :style="{ color: buttonColor }"
-            class="closeButton" @click="deleteImage">&#10006;</button>
-    
-    <img v-if="isItDefaultImage" class="defaultImage" :src="defaultImageURL" alt="Default Image">
-    <h1 v-if="isItDefaultImage">
-      Import image
-    </h1>
-    <img v-else class="uploadedImage" :src="uploadedImageURL">
+  <div class="row">
+    <div class="workArea">
+      <button v-if="!isItDefaultImage" :style="{ color: buttonColor }"
+              class="closeButton" @click="deleteImage">&#10006;</button>
+      
+      <img v-if="isItDefaultImage" class="defaultImage" :src="defaultImageURL" alt="Default Image">
+      <h1 v-if="isItDefaultImage">
+        Import image
+      </h1>
+      <img v-else class="uploadedImage" :src="uploadedImageURL">
 
-    <button class="uploadButton" @click="triggerFileInput">
-      Upload Image
+      <button class="uploadButton" @click="triggerFileInput">
+        Upload Image
+      </button>
+
+      <input type="file" ref="fileInput" @change="uploadImage" accept="image/*" class="uploadInput" style="display: none;" />
+    </div>
+
+    <div class="workArea">
+
+    </div>
+
+  </div>
+  <div class="row">
+    <button class="startButton" @click="sendImageToAPI" :disabled="!uploadedImageURL">
+    Start
     </button>
+  </div>
 
-    <input type="file" ref="fileInput" @change="uploadImage" accept="image/*" class="uploadInput" style="display: none;" />
+  <div class="row">
+
+    <div class="workArea">
+
+    </div>  
+
+    <div class="workArea">
+
+    </div>
+
   </div>
 </template>
+
 
 <script>
 export default {
@@ -24,7 +49,7 @@ export default {
       defaultImageURL: "https://icons.iconarchive.com/icons/icons8/windows-8/128/Files-Word-icon.png",
       uploadedImageURL: "",
       isItDefaultImage: true,
-      buttonColor: 'black' // начальный цвет кнопки
+      buttonColor: 'black' // херь, чтоб кнопка меняла цвет
     }
   },
   methods: {
@@ -37,7 +62,7 @@ export default {
       reader.onload = (e) => {
         this.uploadedImageURL = e.target.result;
         this.isItDefaultImage = false;
-        this.determineButtonColor(e.target.result); // вычисляем цвет кнопки
+        this.determineButtonColor(e.target.result); // изменение цвета кнопки
       };
       if (file) {
         reader.readAsDataURL(file);
@@ -47,18 +72,16 @@ export default {
       const img = new Image();
       img.src = imageData;
       img.onload = () => {
-        // Создаем canvas для получения данных пикселей
         const canvas = document.createElement('canvas');
         const ctx = canvas.getContext('2d');
         canvas.width = img.width;
         canvas.height = img.height;
         ctx.drawImage(img, 0, 0);
         
-        // Получаем данные пикселей
         const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
         const data = imageData.data;
 
-        // Вычисляем среднюю яркость
+        // вычисление средней яркости
         let r = 0, g = 0, b = 0;
         const pixelCount = data.length / 4;
         for (let i = 0; i < data.length; i += 4) {
@@ -71,29 +94,64 @@ export default {
         g /= pixelCount;
         b /= pixelCount;
         
-        // Яркость по формуле (настраивается по желанию)
+        // формула яркости
         const brightness = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
 
-        // Устанавливаем цвет кнопки
+        // изменение цвета кнопки через тернарник
         this.buttonColor = brightness < 0.5 ? 'white' : 'black';
       };
     },
     deleteImage() {
       this.uploadedImageURL = "";
       this.isItDefaultImage = true;
-      this.buttonColor = 'black'; // сбрасываем цвет кнопки
+      this.buttonColor = 'black'; // сброс цвета кнопки
       this.$refs.fileInput.value = null; 
+    },
+    sendImageToAPI() {
+      // Извлекаем base64 строку изображения
+      const base64Image = this.uploadedImageURL.split(',')[1];
+
+      // Настройка API URL
+      const apiURL = "https://example.com/upload"; // Замените на ваш URL API
+
+      fetch(apiURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ image: base64Image }), // Передать изображение в формат json
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+        alert('Image successfully uploaded!');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        alert('Error uploading image.');
+      });
     }
   }
 }
 </script>
 
 <style scoped>
-.workAreaImportImage {
+
+.row{
+  flex-direction: row;
+  display: flex;
+  margin: 20px;
+  align-items: center;
+  justify-content: center;
+}
+
+.workArea {
   background-color: rgb(241, 241, 241);
   border: 4px solid rgb(179, 179, 179);
   height: 400px;
   width: 600px;
+  margin-left: 10px;;
+  margin-right: 10px;
   border-radius: 40px;
   overflow: hidden;
   position: relative;
@@ -103,7 +161,7 @@ export default {
   justify-content: center;
 }
 
-.workAreaImportImage h1 {
+.workArea h1 {
   color: black;
   opacity: 0.3;
   text-align: center;
@@ -111,8 +169,8 @@ export default {
 }
 
 .defaultImage {
-  height: 75px;
-  width: 75px;
+  height: 100px;
+  width: 100px;
   opacity: 0.3;
   -webkit-user-drag: none;
 }
@@ -144,7 +202,7 @@ export default {
 .uploadedImage {
   height: 100%;
   width: 100%;
-  object-fit: cover;
+  object-fit: contain;
   position: absolute;
   top: 0;
   left: 0;
@@ -164,5 +222,25 @@ export default {
 
 .uploadButton:hover {
   background-color: #3580cf;
+}
+.startButton {
+  height: 50px;
+  width: 100px;
+  border: none;
+  font-size: 20px;
+  border-radius: 16px;
+  background-color: #09af11;
+  color: white;
+  cursor: pointer;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+.startButton:hover {
+  background-color: #109116;
+}
+.startButton:disabled{
+  background-color: gray;
+  cursor: default;
 }
 </style>
