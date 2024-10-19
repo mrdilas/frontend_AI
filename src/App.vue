7f23ba1f25b120d057474bb2c6ba6858d4e391b8
BigBoyBODIA <vbifynty@yandex.ru>
2024-10-19 17:11:31 +0300

    новый стиль и чуть другая логика

--- src/App.vue
+++ src/App.vue
@@ -1,259 +1,130 @@
 <template>
-  
-  <link :href = "fontDefaultUrl" rel="stylesheet">
+  <div class="workArea">
+    <button v-if="!isItDefaultImage" class="closeButton" 
+            @click="deleteImage">&#10006;</button>
+    
+    <img v-if="isItDefaultImage" class="defaultImage" :src="defaultImageURL" alt="Default Image">
+    <h1 v-if="isItDefaultImage">
+      Import image
+    </h1>
+    <img v-else class="uploadedImage" :src="uploadedImageURL">
 
-  <div className="wrapper-container">
-    <div className="wrapper">
-      <div className="image-container">
-        <img v-if="isItDefaultPhoto" :src = "imageDefaultUrl" width="150" height="150" :style= "opacityFile">
-        <h1 v-if="isItDefaultPhoto" className="pacifico-regular">
-          Добавьте <br >
-          изображение
-        </h1>
-        <img :src="imageUrl" alt="Uploaded Image" v-if="imageUrl" className="uploaded-image"/>
-      </div>
-    </div>
-
-    <div className="wrapper_gen">
-      <h1 className="pacifico-regular"> 
-        Что AI опделелил <br >
-        и с чем дальше работать будет
-      </h1>
-    </div>
-  </div>
-  
-  <input type="file" ref="fileInput" @change="handleFileChange" style="display:none;" />
-  
-  <div>
-    <button @click="openFileDialog" className="button">
-      Выбрать фото
+    <button class="uploadButton" @click="triggerFileInput">
+      Upload Image
     </button>
 
-    <button @click="deleteFileChange" className="button_delete" :disabled="isItDefaultPhoto == true">
-      Удалить фото
-    </button>
-
-    <button @click="" className="button" :disabled="isItDefaultPhoto == true"> 
-      Редактировать фото
-    </button>
-  
-    <button className="button_gen" :disabled="isItDefaultPhoto == true">
-      Генерация объекта
-    </button>
-  </div>
-
-  <div className="wrapper-container">
-    <div className="wrapper">
-      
-      <h1 className="pacifico-regular"> 
-        Здесь будет <br >
-        3D модель</h1>
-    </div>
-    <div className="wrapper_gen">
-      <h1 className="pacifico-regular">
-        Модель, интегрированная <br>
-        в окружение
-      </h1>
-    </div>
+    <input type="file" ref="fileInput" @change="uploadImage" accept="image/*" class="uploadInput" style="display: none;" />
   </div>
 </template>
-
-
 
 <script>
 export default {
   data() {
     return {
-      imageDefaultUrl: "https://icons.iconarchive.com/icons/icons8/windows-8/128/Files-Word-icon.png", //ссылка на иконку файла
-      fontDefaultUrl: "https://fonts.googleapis.com/css2?family=Pacifico&display=swap", // ссылка на шрифт для текста
-      imageUrl: '',
-      isItDefaultPhoto: true, // если нет фотографии выводится иконка и текст в первом контейнере 
-      opacityFile: { opacity: 0.2 }, //прозрачность для иконки
-      imageClose: 'https://icons.iconarchive.com/icons/custom-icon-design/mono-general-1/128/delete-icon.png',
-      isItDefaultPhoto: true,
-    };
+      defaultImageURL: "https://icons.iconarchive.com/icons/icons8/windows-8/128/Files-Word-icon.png",
+      uploadedImageURL: "",
+      isItDefaultImage: true
+    }
   },
   methods: {
-    // функция для выбора картинки
-    openFileDialog() {
+    triggerFileInput() {
+      // Программно вызываем файловый input
       this.$refs.fileInput.click();
     },
-    
-    // функция для добавление в imageUrl выбранной картинки
-    handleFileChange(event) {
+    uploadImage(event) {
       const file = event.target.files[0];
-      // Поддерживаемые форматы изображений
-      const supportedFormats = ['image/jpeg', 'image/png', 'image/bmp', 'image/webp'];
-      // Проверка типа файла
-      if (file && supportedFormats.includes(file.type)) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          this.imageUrl = e.target.result;
-          this.isItDefaultPhoto = false;
-        };
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.uploadedImageURL = e.target.result;
+        this.isItDefaultImage = false;
+      };
+      if (file) {
         reader.readAsDataURL(file);
-      } else {
-            alert('Пожалуйста, выберите изображение в формате JPEG, PNG, BMP или WEBP.');
       }
     },
-
-    // функция для удаления картинки из imageUrl
-    deleteFileChange() {
-      this.imageUrl = '';
-      this.isItDefaultPhoto = true;
-      this.$refs.fileInput.value = ''; // Сбрасываем значение input, чтобы можно было заново выбрать тот же файл
-    },
-  },
-};
+    deleteImage() {
+      this.uploadedImageURL = "";
+      this.isItDefaultImage = true;
+    }
+  }
+}
 </script>
 
 <style scoped>
-/* тут нахуй без комментариев, я сам уже не шарю
-  что где, ищите по наитию */
-.wrapper-container {
+.workArea {
+  background-color: rgb(241, 241, 241);
+  border: 4px solid rgb(179, 179, 179);
+  height: 400px;
+  width: 600px;
+  border-radius: 40px;
+  overflow: hidden;
+  position: relative;
   display: flex;
-  pointer-events: none;
-  justify-content: center;
-  margin-top: 20px;
-}
-.wrapper {
-  width: 900px; /* 900px */
-  height: 500px; /* 500px*/
-  border-radius: 20px;
-  background: rgb(240, 230, 213);
-  margin: 0 20px;
-  display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  overflow: hidden; /* Обрезаем все, что выходит за пределы контейнера */
-  object-fit: contain;  /* Сохраняем пропорции изображений */
-}
-
-.image-container {
-  width: 100%; /* Полная ширина контейнер с изображением */
-  height: 100%; /* Полная высота контейнера с изображением */
-  display: flex; /* Используем Flexbox для центрального выравнивания */
-  justify-content: center; /* Центрируем содержимое по горизонтали */
-  align-items: center; /* Центрируем содержимое по вертикали */
-  overflow: hidden; /* Скрываем все, что выходит за пределы */
-} 
-
-.img_file { /* Стили для всех изображений в .wrapper */
-  width: 150px; /* Ширина изображения не больше ширины родителя */
-  height: 150px; /* Высота изображения не больше высоты родителя */
-}
-.uploaded-image {
-  max-width: 100%;     /* Ширина для загруженных изображений не больше ширины родителя */
-  max-height: 100%;    /* Высота для загруженных изображений не больше высоты родителя */
-  object-fit: contain;  /* Сохраняем пропорции изображений */
-}
-
-.wrapper_gen {
-  width: 900px;
-  height: 500px;
-  border-radius: 20px;
-  background: rgb(240, 230, 213);
-  margin: 0 20px;
   justify-content: center;
-  align-items: center;
-  display: flex;
 }
 
-button {
-  background: rgb(241, 173, 46);
-  color: white;
-  border-radius: 10px;
-  border: 2px solid rgb(245, 245, 115);
-  margin-left: 20px;
-  padding: 10px 15px;
-  cursor: pointer;
-  transition: transform 500ms ease;
-  margin-top: 20px;
-  font-size: 16px;
-}
-button:hover {
-  transform: scale(1.1) translateY(-5px);
-}
-
-
-.button:disabled {
-  background-color: gray;
-  cursor: not-allowed;
-  opacity: 0.2;
-  transform: scale(1) translateY(0px);
-}
-
-.button_close_image {
-  height: 10px;
-  width: 10px;
-  border-color: black;
-  background-color: white;
-  float: right;
-  cursor: pointer;
-}
-
-.button_gen {
-  background: rgb(30, 128, 17);
-  color: white;
-  border-radius: 10px;
-  border: 2px solid rgb(18, 77, 10);
-  padding: 10px 15px;
-  cursor: pointer;
-  transition: transform 500ms ease;
-  font-size: 16px;
-}
-.button_gen:hover {
-  transform: scale(1.1) translateY(-5px);
-}
-.button_gen:disabled {
-  background-color: gray;
-  cursor: not-allowed;
-  opacity: 0.2;
-  transform: scale(1) translateY(0px);
-}
-
-.button_delete {
-  background: rgb(212, 30, 30);
-  color: white;
-  border-radius: 10px;
-  border: 2px solid rgb(110, 4, 4);
-  margin-left: 20px;
-  padding: 10px 15px;
-  cursor: pointer;
-  transition: transform 500ms ease;
-  margin-top: 20px;
-  font-size: 16px;
-}
-.button_delete:hover {
-  transform: scale(1.1) translateY(-5px);
-}
-.button_delete:disabled {
-  background-color: gray;
-  cursor: not-allowed;
-  opacity: 0.2;
-  transform: scale(1) translateY(0px);
-}
-
-.image-container {
-  width: 100%; /* Полная ширина контейнер с изображением */
-  height: 100%; /* Полная высота контейнера с изображением */
-  display: flex; /* Используем Flexbox для центрального выравнивания */
-  justify-content: center; /* Центрируем содержимое по горизонтали */
-  align-items: center; /* Центрируем содержимое по вертикали */
-  overflow: hidden; /* Скрываем все, что выходит за пределы */
-}
-.img_file {
-  opacity: 0.2;
-}
-
-.pacifico-regular {
-  font-family: "Pacifico", cursive;
-  font-weight: 400;
-  font-style: normal;
-  text-align: center;
-  font-size: 45px;
+.workArea h1 {
+  color: black;
   opacity: 0.3;
+  text-align: center;
+  z-index: 2;
 }
 
-</style>
+.defaultImage {
+  height: 75px;
+  width: 75px;
+  opacity: 0.3;
+  -webkit-user-drag: none;
+}
 
+.closeButton {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  width: 45px;
+  height: 45px;
+  border: none;
+  border-radius: 10px;
+  color: black;
+  background-color: white;
+  font-size: 40px;
+  cursor: pointer;
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+}
+
+.closeButton:focus {
+  outline: none;
+}
+
+.uploadedImage {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  -webkit-user-drag: none;
+}
+
+.uploadButton {
+  margin-top: 20px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 16px;
+  background-color: #007BFF;
+  color: white;
+  cursor: pointer;
+}
+
+.uploadButton:hover {
+  background-color: #3580cf;
+}
+</style>
