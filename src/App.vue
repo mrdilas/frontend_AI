6300f8c514f10673c4f9e68441e6f0fa895802ad
BigBoyBODIA <vbifynty@yandex.ru>
2024-10-19 21:18:34 +0300

    решил небольшую шнягу с кнопкой (Данил, посмотри)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="workArea">
-    <button v-if="!isItDefaultImage" class="closeButton" 
-            @click="deleteImage">&#10006;</button>
+  <div class="workAreaImportImage">
+    <button v-if="!isItDefaultImage" :style="{ color: buttonColor }"
+            class="closeButton" @click="deleteImage">&#10006;</button>
     
     <img v-if="isItDefaultImage" class="defaultImage" :src="defaultImageURL" alt="Default Image">
     <h1 v-if="isItDefaultImage">
@@ -23,12 +23,12 @@ export default {
     return {
       defaultImageURL: "https://icons.iconarchive.com/icons/icons8/windows-8/128/Files-Word-icon.png",
       uploadedImageURL: "",
-      isItDefaultImage: true
+      isItDefaultImage: true,
+      buttonColor: 'black' // начальный цвет кнопки
     }
   },
   methods: {
     triggerFileInput() {
-      // Программно вызываем файловый input
       this.$refs.fileInput.click();
     },
     uploadImage(event) {
@@ -37,21 +37,59 @@ export default {
       reader.onload = (e) => {
         this.uploadedImageURL = e.target.result;
         this.isItDefaultImage = false;
+        this.determineButtonColor(e.target.result); // вычисляем цвет кнопки
       };
       if (file) {
         reader.readAsDataURL(file);
       }
     },
+    determineButtonColor(imageData) {
+      const img = new Image();
+      img.src = imageData;
+      img.onload = () => {
+        // Создаем canvas для получения данных пикселей
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+        
+        // Получаем данные пикселей
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = imageData.data;
+
+        // Вычисляем среднюю яркость
+        let r = 0, g = 0, b = 0;
+        const pixelCount = data.length / 4;
+        for (let i = 0; i < data.length; i += 4) {
+          r += data[i];
+          g += data[i + 1];
+          b += data[i + 2];
+        }
+
+        r /= pixelCount;
+        g /= pixelCount;
+        b /= pixelCount;
+        
+        // Яркость по формуле (настраивается по желанию)
+        const brightness = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+
+        // Устанавливаем цвет кнопки
+        this.buttonColor = brightness < 0.5 ? 'white' : 'black';
+      };
+    },
     deleteImage() {
       this.uploadedImageURL = "";
       this.isItDefaultImage = true;
+      this.buttonColor = 'black'; // сбрасываем цвет кнопки
+      this.$refs.fileInput.value = null; 
     }
   }
 }
 </script>
 
 <style scoped>
-.workArea {
+.workAreaImportImage {
   background-color: rgb(241, 241, 241);
   border: 4px solid rgb(179, 179, 179);
   height: 400px;
@@ -65,7 +103,7 @@ export default {
   justify-content: center;
 }
 
-.workArea h1 {
+.workAreaImportImage h1 {
   color: black;
   opacity: 0.3;
   text-align: center;
@@ -88,7 +126,7 @@ export default {
   border: none;
   border-radius: 10px;
   color: black;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 0);;
   font-size: 40px;
   cursor: pointer;
   z-index: 3;
